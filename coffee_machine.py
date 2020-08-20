8082ca220aad26d6e200445d9c7a166365a00cd6
Arvindkumar S P <arvindkumar1998ece@gmail.com>
2020-08-20 13:41:26 +0530

    First Project

--- coffee_machine.py
+++ coffee_machine.py
@@ -14,55 +14,71 @@ class CoffeeMachine:  # Define the class CoffeeMachine
     def buy(self):
         print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
         self.choose = input()
-        if self.choose == '1':  # To make espresso 250ml water, 16g coffee beans, $4 needed
-            if self.initial_water < 250:
-                print("Sorry, not enough water")
-            elif self.initial_coffee_bean < 16:
-                print("Sorry, not enough coffee beans")
-            elif self.initial_disposal_cup < 1:
-                print("Sorry, not enough disposable cups")
+        while True:
+            if self.choose == '1':  # To make espresso 250ml water, 16g coffee beans, $4 needed
+                if self.initial_water < 250:
+                    print("Sorry, not enough water")
+                    break
+                elif self.initial_coffee_bean < 16:
+                    print("Sorry, not enough coffee beans")
+                    break
+                elif self.initial_disposal_cup < 1:
+                    print("Sorry, not enough disposable cups")
+                    break
+                else:
+                    print("I have enough resources, making you a coffee!")
+                    self.initial_water -= 250
+                    self.initial_coffee_bean -= 16
+                    self.initial_disposal_cup -= 1
+                    self.initial_cash += 4
+                    break
+            elif self.choose == '2':  # To make latte 350ml water, 75ml of milk, 20g coffee beans, $7 needed
+                if self.initial_water < 350:
+                    print("Sorry, not enough water")
+                    break
+                elif self.initial_milk < 75:
+                    print("Sorry, not enough milk")
+                    break
+                elif self.initial_coffee_bean < 16:
+                    print("Sorry, not enough coffee beans")
+                    break
+                elif self.initial_disposal_cup < 1:
+                    print("Sorry, not enough disposable cups")
+                    break
+                else:
+                    print("I have enough resources, making you a coffee!")
+                    self.initial_water -= 350
+                    self.initial_milk -= 75
+                    self.initial_coffee_bean -= 20
+                    self.initial_disposal_cup -= 1
+                    self.initial_cash += 7
+                    break
+            elif self.choose == '3':  # To make cappuccino 200ml water, 100ml of milk, 12g coffee beans, $6 needed
+                if self.initial_water < 200:
+                    print("Sorry, not enough water")
+                    break
+                elif self.initial_milk < 100:
+                    print("Sorry, not enough milk")
+                    break
+                elif self.initial_coffee_bean < 12:
+                    print("Sorry, not enough coffee beans")
+                    break
+                elif self.initial_disposal_cup < 1:
+                    print("Sorry, not enough disposable cups")
+                    break
+                else:
+                    print("I have enough resources, making you a coffee!")
+                    self.initial_water -= 200
+                    self.initial_milk -= 100
+                    self.initial_coffee_bean -= 12
+                    self.initial_disposal_cup -= 1
+                    self.initial_cash += 6
+                    break
+            elif self.choose == 'back':
+                break
             else:
-                print("I have enough resources, making you a coffee!")
-                self.initial_water -= 250
-                self.initial_coffee_bean -= 16
-                self.initial_disposal_cup -= 1
-                self.initial_cash += 4
-        elif self.choose == '2':  # To make latte 350ml water, 75ml of milk, 20g coffee beans, $7 needed
-            if self.initial_water < 350:
-                print("Sorry, not enough water")
-            elif self.initial_milk < 75:
-                print("Sorry, not enough milk")
-            elif self.initial_coffee_bean < 16:
-                print("Sorry, not enough coffee beans")
-            elif self.initial_disposal_cup < 1:
-                print("Sorry, not enough disposable cups")
-            else:
-                print("I have enough resources, making you a coffee!")
-                self.initial_water -= 350
-                self.initial_milk -= 75
-                self.initial_coffee_bean -= 20
-                self.initial_disposal_cup -= 1
-                self.initial_cash += 7
-        elif self.choose == '3':  # To make cappuccino 200ml water, 100ml of milk, 12g coffee beans, $6 needed
-            if self.initial_water < 200:
-                print("Sorry, not enough water")
-            elif self.initial_milk < 100:
-                print("Sorry, not enough milk")
-            elif self.initial_coffee_bean < 12:
-                print("Sorry, not enough coffee beans")
-            elif self.initial_disposal_cup < 1:
-                print("Sorry, not enough disposable cups")
-            else:
-                print("I have enough resources, making you a coffee!")
-                self.initial_water -= 200
-                self.initial_milk -= 100
-                self.initial_coffee_bean -= 12
-                self.initial_disposal_cup -= 1
-                self.initial_cash += 6
-        elif self.choose == 'back':
-            pass
-        else:
-            print("Please enter the correct input")
+                print("Please enter the correct input")
+                break
 
     def fill(self):
         self.fill_water = int(input("Write how many ml of water do you want to add:"))
